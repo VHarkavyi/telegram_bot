c7f38122f9e2a071d4176ce6dac4dd87dd05b19e
Vitalii_Harkavyi <vgarry12@gmail.com>
2023-04-08 16:01:13 +0300

    Add_spend finished and ready

--- Bot.py
+++ Bot.py
@@ -9,12 +9,23 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 bot = telebot.TeleBot('6133056354:AAH7DGv0JOw1Tny3_DB7mcnbY4VHmBCPFNY')
 
 ############################################################################
-# Создание кнопок
+# Стартовыe кнопки
 keyboard_start = ReplyKeyboardMarkup(resize_keyboard=True)
 add_spend_button = KeyboardButton('Добавить трату')
 balance_button = KeyboardButton('Баланс')
 keyboard_start.add(add_spend_button)
 keyboard_start.add(balance_button)
+
+# Кнопки вернуться назад
+keyboard_back_to_start = ReplyKeyboardMarkup(resize_keyboard=True)
+back_to_start_button = KeyboardButton('Вернуться на старт')
+keyboard_back_to_start.add(back_to_start_button)
+
+# Кнопка пропуска + вернуться назад
+keyboard_skip = ReplyKeyboardMarkup(resize_keyboard=True)
+skip_button = KeyboardButton('Пропустить')
+keyboard_skip.add(skip_button)
+keyboard_skip.add(back_to_start_button)
 
 # кнопки категорий
 Groceries = KeyboardButton('Groceries')
@@ -35,6 +46,7 @@ Events = KeyboardButton('Events')
 Save = KeyboardButton('Save')
 
 keyboard_category = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_category.add(back_to_start_button)
 keyboard_category.add(Groceries)
 keyboard_category.add(Javelina)
 keyboard_category.add(Homeware_Hygiene)
@@ -60,6 +72,7 @@ Ukrsib = KeyboardButton('Ukrsib')
 Privat = KeyboardButton('Privat')
 
 keyboard_source = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_source.add(back_to_start_button)
 keyboard_source.add(MonoBlack)
 keyboard_source.add(MonoWhite)
 keyboard_source.add(Cash)
@@ -128,48 +141,76 @@ def select_category(message):
     user_state = user_global_state.get(message.chat.id)
     if not user_state or user_state['step'] != 'select_category':
         return
-    category = message.text
-    user_global_state[message.chat.id]['category'] = category
-    user_global_state[message.chat.id]['step'] = 'select_source'
-    print(user_global_state)
-    bot.send_message(message.chat.id, 'Выберите источник: ', reply_markup=keyboard_source)
+    if message.text == 'Вернуться на старт':
+        start_handler(message)
+        user_global_state[message.chat.id] = {'step': 'start'}
+        print(user_global_state)
+    else:
+        category = message.text
+        user_global_state[message.chat.id]['category'] = category
+        user_global_state[message.chat.id]['step'] = 'select_source'
+        print(user_global_state)
+        bot.send_message(message.chat.id, 'Выберите источник: ', reply_markup=keyboard_source)
 
 @bot.message_handler(func=lambda message: message.content_type == 'text' and user_global_state.get(message.chat.id, {}).get('step') == 'select_source')
 def select_source(message):
     user_state = user_global_state.get(message.chat.id)
     if not user_state or user_state['step'] != 'select_source':
         return
-    source = message.text
-    user_global_state[message.chat.id]['source'] = source
-    user_global_state[message.chat.id]['step'] = 'enter_amount'
-    print(user_global_state)
-    bot.send_message(message.chat.id, 'Введите сумму:', reply_markup=ReplyKeyboardRemove())
+    if message.text == 'Вернуться на старт':
+        start_handler(message)
+        user_global_state[message.chat.id] = {'step': 'start'}
+        print(user_global_state)
+    else:
+        source = message.text
+        user_global_state[message.chat.id]['source'] = source
+        user_global_state[message.chat.id]['step'] = 'enter_amount'
+        print(user_global_state)
+        # bot.send_message(message.chat.id, 'Введите сумму:', reply_markup=ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, 'Введите сумму:', reply_markup=keyboard_back_to_start)
 
 @bot.message_handler(func=lambda message: message.content_type == 'text' and user_global_state.get(message.chat.id, {}).get('step') == 'enter_amount')
 def enter_amount(message):
     user_state = user_global_state.get(message.chat.id)
     if not user_state or user_state['step'] != 'enter_amount':
         return
-    try:
-        amount = float(message.text.replace(',', '.'))
-        user_global_state[message.chat.id]['amount'] = amount
-        user_global_state[message.chat.id]['step'] = 'enter_comment'
+    if message.text == 'Вернуться на старт':
+        start_handler(message)
+        user_global_state[message.chat.id] = {'step': 'start'}
         print(user_global_state)
-        bot.send_message(message.chat.id, f"Добавьте комментарий: ")
-    except ValueError:
-        bot.send_message(message.chat.id, "Ошибка! Введите число.")
+    else:
+        try:
+            amount = float(message.text.replace(',', '.'))
+            user_global_state[message.chat.id]['amount'] = amount
+            user_global_state[message.chat.id]['step'] = 'enter_comment'
+            print(user_global_state)
+            bot.send_message(message.chat.id, f"Добавьте комментарий: ", reply_markup=keyboard_skip)
+        except ValueError:
+            bot.send_message(message.chat.id, "Ошибка! Введите число.")
 
 @bot.message_handler(func=lambda message: message.content_type == 'text' and user_global_state.get(message.chat.id, {}).get('step') == 'enter_comment')
 def add_comment(message):
     user_state = user_global_state.get(message.chat.id)
     if not user_state or user_state['step'] != 'enter_comment':
         return
-    comment = message.text
-    now = datetime.datetime.now()
-    data = now.strftime("%d-%b")
-    spend_entry = [user_state['category'], user_state['source'], int(user_state['amount']), data, comment]
-    write_data_to_file(spend_entry, message)
-    user_global_state[message.chat.id] = {'step': 'start'}
+    if message.text == 'Вернуться на старт':
+        start_handler(message)
+        user_global_state[message.chat.id] = {'step': 'start'}
+        print(user_global_state)
+    elif message.text == 'Пропустить':
+        comment = ' '
+        now = datetime.datetime.now()
+        data = now.strftime("%d-%b")
+        spend_entry = [user_state['category'], user_state['source'], int(user_state['amount']), data, comment]
+        write_data_to_file(spend_entry, message)
+        user_global_state[message.chat.id] = {'step': 'start'}
+    else:
+        comment = message.text
+        now = datetime.datetime.now()
+        data = now.strftime("%d-%b")
+        spend_entry = [user_state['category'], user_state['source'], int(user_state['amount']), data, comment]
+        write_data_to_file(spend_entry, message)
+        user_global_state[message.chat.id] = {'step': 'start'}
 
 ###########################################################################################
 
